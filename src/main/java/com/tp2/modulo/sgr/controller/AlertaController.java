c3f04e5b51ebf5fe75a215bf4860d4576c273c4f
Omar Diaz <omardiaze@gmail.com>
2018-11-14 18:45:53 -0500

    Actualización de envio de correo

--- src/main/java/com/tp2/modulo/sgr/controller/AlertaController.java
+++ src/main/java/com/tp2/modulo/sgr/controller/AlertaController.java
@@ -18,13 +18,25 @@ import javax.ws.rs.Path;
 import com.tp2.modulo.sgr.model.RespuestaResponse;
 import com.tp2.modulo.sgr.service.AlertaService;
 
+
 @Path("/alertas")
 public class AlertaController {
-
-		AlertaService AlertaService = new AlertaService();
+	
+	@GET
+	@Path("/{correo}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAlertas(@PathParam("correo") String correo) {
+		
+		AlertaService alertaService = new AlertaService();		
+		alertaService.sendSimpleMessagesv2(correo, "Notificación de riesgo","cuerpo", null);
+		
 		Gson gson = new Gson();
+		
+		return gson.toJson("ok");
+		
+	}
 		
 	
 		
 
-	}
+}
--- src/main/java/com/tp2/modulo/sgr/service/AlertaService.java
+++ src/main/java/com/tp2/modulo/sgr/service/AlertaService.java
@@ -39,7 +39,7 @@ public class AlertaService {
 		}
 
 		public void sendSimpleMessageUsingTemplate(String to, String subject, SimpleMailMessage template,
-				String... templateArgs) {
+			String... templateArgs) {
 			String text = String.format(template.getText(), templateArgs);
 			sendSimpleMessage(to, subject, text);
 		}
@@ -65,18 +65,23 @@ public class AlertaService {
 		
 		public void sendSimpleMessagesv2(String to, String subject, String text, List<Correo> co) {
 			try {
+				
+				String body ="<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\"><head></head><body><style></style><article class=\"bodymail\" style=\"background-color:#eee;padding:10px 20px\"><h2 class=\"mailtitle\" style=\"color:#333;font-size:20px;text-align:center\">Modulo Riesgos</h2><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\">Notificación de riesgos, usted recibe este correo por estar asignado como responsable de ...</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\">" + text + "</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><b>Nombre del riesgo:</b> nombre del riesgo</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><b>Descripción:</b> descripción del riesgo</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><b>Persona que identificó:</b> Omar Diaz</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><b>Fecha de registro:</b> 15/11/2018</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><b>Tipo de escala:</b> Costo</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><b>Nivel del riesgo:</b> Medio</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><b>Costo:</b> S/ 500.00</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><b>Probabilidad:</b> 0.8</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\">&nbsp;</p><p class=\"mailparagraph\" style=\"font-size:14px;margin:10px\"><img src=\"http://riesgos-backend.appspot.com/img/logo.png\" alt=\"\"></p></article></body></html>";
 				 
 				JavaMailSenderImpl emailSender = new JavaMailSenderImpl();
 				
-				emailSender.setUsername("testmail@nextsmart.net");
-				emailSender.setPassword("X.W.~GICK@*)RPIAV]");
+//				emailSender.setUsername("testmail@nextsmart.net");
+//				emailSender.setPassword("X.W.~GICK@*)RPIAV]");
+				emailSender.setUsername("riesgos-api@sandboxef21ca99a1644f77b4087f19cb8c6fed.mailgun.org");
+				emailSender.setPassword("123456789");
 				
 				Properties properties = new Properties();
 			    properties.setProperty("mail.transport.protocol", "smtp");
 			    properties.setProperty("mail.smtp.auth", "true");
 			    properties.setProperty("mail.smtp.starttls.enable", "false");
 //			    properties.setProperty("mail.debug", Boolean.toString(this.debug));
-			    properties.setProperty("mail.smtp.host", "nextsmartservers.com");
+//			    properties.setProperty("mail.smtp.host", "nextsmartservers.com");
+			    properties.setProperty("mail.smtp.host", "smtp.mailgun.org");
 			    properties.setProperty("mail.smtp.port", "25");
 //			    properties.setProperty("mail.smtp.ssl.trust", this.trust);
 			    emailSender.setJavaMailProperties(properties);
@@ -85,11 +90,14 @@ public class AlertaService {
 				MimeMessage message = emailSender.createMimeMessage(); 
 //				MimeMessageHelper helper = new MimeMessageHelper(message);
 				MimeMessageHelper helper = new MimeMessageHelper(message,true);
+				
+				message.setContent(body, "text/html");
 
 //				SimpleMailMessage message = new SimpleMailMessage();
 				try {
 //					helper.setFrom(getProp("spring.mail.from_global"),getProp("spring.mail.from_global_name"));
-					helper.setFrom("djuarez7@hotmail.com","Administrador de correos");
+//					helper.setFrom("djuarez7@hotmail.com","Administrador de correos");
+					helper.setFrom("riesgos-api@sandboxef21ca99a1644f77b4087f19cb8c6fed.mailgun.org","Administrador de correos");
 				} catch (Exception e) {
 					// TODO: handle exception
 				}
